c0f25fda96f6aaf2d935a21d73213bd345e26744
Nihal Donderici <nihaldonderici@gmail.com>
2022-03-29 20:29:48 -0400

    Added First file

--- src/day0/JamesBond.java
+++ src/day0/JamesBond.java
@@ -3,6 +3,6 @@ package day0;
 public class JamesBond{
   
   public static void main(String [] args){
-    System.out.println("I love James Bond);
+    System.out.println("I love James Bond");
   }
 }
